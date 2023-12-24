e0deba26f354dac4e11385c2ddca33c66afa7a34
Thara Paidipati <tharapaidipati1@gmail.com>
2023-12-24 14:19:21 +0000

    adding first programes

---
+++ Day 2 program.py
@@ -0,0 +1,44 @@
+food = "cake"
+f"I like {food}"
+
+feeling = "happy"
+print(f"I'm so {feeling} to be learning how to code Python!")
+
+multiline_sentence = """
+    Here is
+    a sentence
+    on many
+    different lines.
+
+"""
+print(f"{multiline_sentence}")
+
+print("Hi! My name is Thara.")
+
+print("\"Coding is a superpower! You can do so many cool things with your imagination and code.\" -Thara Paidipati")
+
+feeling = "excited"
+print(f"Today I feel {feeling} to code!")
+
+print("Thara P loves \nstudying, coding Python, \nand swimming a lot")
+
+haiku = """
+    Thara P loves
+    studying, coding Python,
+    and swimming, a lot
+"""
+print(f"{haiku}")
+
+
+name = "Thara"
+adjective = "spicy"
+favorite_snack = "crisps"
+number = "2"
+type_of_tree = "mango tree"
+
+silly_story = f"""
+    Hi, my name is {name}.
+    I really like {adjective} {favorite_snack}!
+    I like it so much, I try ti eat it at least {number} times every day.
+    It tastes even better when you eat it under a {type_of_tree}!
+"""
