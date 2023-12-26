3d0a78007b1ee13507c815e12b548cdb7381f0f0
thara-paidipati <tharapaidipati1@gmail.com>
2023-12-26 22:10:34 +0000

    remaned files and added day3 and 4 files

---
+++ day1program.py
@@ -0,0 +1,25 @@
+# print("I should not be printed!")
+
+print("Hello")
+# print("You are a silly shoe!")
+
+# This code prints out text to the shell
+print("Hello there!")
+
+print("I'm so hapy to be learning how to code in Python!")
+
+print('I\'m so happy to be learning how to code in Python!')
+
+print("\"Kumusta\" is \"Hello\" in Tagalog!")
+
+print("Here is \na sentence \non many \ndifferent lines.")
+
+reader = "Casey"
+print(reader)
+
+favorite_number = 3
+type(favorite_number)
+
+favorite_number = "3"
+type(favorite_number)
+      
---
+++ day3programme.py
@@ -0,0 +1,12 @@
+first_name = "Thara"
+full_name = "Thara Paidipati"
+print(f"{full_name}")
+
+first_name = "Thara"
+favorite_cookie = "Chocolate chip cookie"
+favorite_number = "20"
+favorite_color = "Blue"
+full_name = "Thara Paidipati"
+occupation = "Student"
+print(f"{first_name} {favorite_cookie} {favorite_number} {favorite_color} {full_name} {occupation}")
+
