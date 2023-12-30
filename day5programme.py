466a3bd54087f39825bd1285ab982bffcc7884c0
thara-paidipati <tharapaidipati1@gmail.com>
2023-12-30 21:06:54 +0000

    adding day5 and 6 files

---
+++ day5programme.py
@@ -0,0 +1,17 @@
+pizza_has_pepperoni = True
+pizza_has_mushrooms = False
+
+pizza_has_pepperoni and pizza_has_mushrooms 
+
+pizza_has_pepperoni or pizza_has_mushrooms
+
+name = "Thara"
+age = 10 + 3
+print( f"Hi there! My name is {name} and I am {age} years old!")
+
+a = 3
+b = 5
+c = 190
+d = 100
+magic_number = (3 ** 5) + (190 % 100)
+print(f"{magic_number}")
---
+++ day6programme.py
@@ -0,0 +1,74 @@
+dolores_chocolate_chips = 13
+teddy_chocolate_chips = 9
+print(f"Teddy's cookie has more chocolate chips than Dolores's. This is {teddy_chocolate_chips > dolores_chocolate_chips}!")
+
+rey_chocolate_chips = 10
+finn_chocolate_chips = 18
+print(f"Rey's cookie has less than or the same amount of chocolate chips as Finn. This is {rey_chocolate_chips < finn_chocolate_chips}!")
+
+tom_chocolate_chips = 50
+jerry_chocolate_chips = "50"
+print(f"Tom does not have the same amount of chocolate chips as Jerry. This is {tom_chocolate_chips != jerry_chocolate_chips}!")
+
+neo_chocolate_chips = 3
+trinity_chocolate_chips = 3
+print(f"Neo has the same number of chocolate chips as Trinity. This is {neo_chocolate_chips == jerry_chocolate_chips}!")
+
+kiki_chocolate_chips = 30
+gigi_chocolate_chips = 31
+print(f"Kiki has lesser chocolate chips than Gigi. This is {kiki_chocolate_chips < gigi_chocolate_chips}!")
+
+total_people = 124
+graham_cracker_crust_lovers = 40
+vanilla_wafer_crust_lovers = 64
+oreo_crust_lovers = 20
+
+pie_crust = "graham cracker"
+pie_slices = 10
+can_evenly_divide_chocolate_caramel_pie = (graham_cracker_crust_lovers % 10) == 0
+print(f"The Chocolate and Caramel pie can be evenly divided for all graham crust lovers? {can_evenly_divide_chocolate_caramel_pie}")
+
+pie_crust = "vanilla wafer"
+pie_slices = 12
+can_evenly_divide_triple_berry_pie = (vanilla_wafer_crust_lovers % 12) == 0
+print(f"The Triple Berry pie can be evenly divided for al vanilla wafer crust lovers? {can_evenly_divide_triple_berry_pie}")
+
+pie_crust = "graham cracker"
+pie_slices = 12
+can_evenly_divide_pumpkin_pie = (graham_cracker_crust_lovers % 12) == 0
+print(f"The Pumpkin pie can be evenly divided for all graham crust lovers? {can_evenly_divide_pumpkin_pie}")
+
+pie_crust = "vanilla wafer"
+pie_slices = 10
+can_evenly_divide_apple_pie = (vanilla_wafer_crust_lovers % 10) == 0
+print(f"The Apple pie can be eveny divided for all vanilla wafer crust lovers? {can_evenly_divide_apple_pie}")
+
+pie_crust = "vanilla wafer"
+pie_slices = 10
+can_evenly_divide_banana_cream_pie = (vanilla_wafer_crust_lovers % 10) == 0
+print(f"The Banana and Cream pie can be evenly divided for all vanilla crust lovers? {can_evenly_divide_banana_cream_pie}")
+
+pie_crust = "graham cracker"
+pie_slices = 12
+can_evenly_divide_mango_pie = (graham_cracker_crust_lovers % 12) == 0
+print(f"The Mango pie can be evenly divided for all graham crust lovers? {can_evenly_divide_mango_pie}")
+
+pie_crust = "oreo"
+pie_slices = 12
+can_evenly_divide_marshmellow_pie = (oreo_crust_lovers % 12) == 0
+print(f"The Marshmellow pie can be evenly divided for all oreo crust lovers? {can_evenly_divide_marshmellow_pie}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
