c1f59a42883913674f136da0362261fad8075f7a
thara-paidipati <tharapaidipati1@gmail.com>
2024-01-02 17:17:38 +0000

    adding day8 file

---
+++ day8programme.py
@@ -0,0 +1,54 @@
+print(4364 % 5)
+print(765 % 8)
+
+
+print(f"The Tripolia galaxy has {9 ** 3} planets!")
+
+print(f"The Deka galaxy has {9 ** 10} planets!")
+
+print(f"The Heptaton galaxy has {9 ** 7} planets!")
+
+print(f"The Oktopia galaxy has {9 ** 8} planets!")
+
+
+#Challenge
+
+#Entrees
+pepperoni_pizza = "91334"
+hamburger = "cheeseburger"
+steak = "0980sdf3"
+pasta = "ribs"
+fried_chicken = "fried chicken"
+
+#Sides
+baked_potato = "mashed potatoes"
+mashed_potatoes = "baked potato"
+french_fries = "french fries"
+mac_n_cheese = "33333"
+steamed_carrots = "green"
+broccoli = "chocolate cips"
+
+#Desserts
+chocolate_ice_cream = "chocolate ice cream"
+strawberry_ice_cream = "vanilla ice cream"
+apple_pie = "pumpkin pie"
+egg_pie = "302948"
+watermelon = "oranges"
+vanilla_donut = "cereal"
+
+name = "Thara"
+entree = fried_chicken
+side_one = french_fries
+side_two = baked_potato
+dessert_one = chocolate_ice_cream
+dessert_two = apple_pie
+dessert_three = vanilla_donut
+dinner_decisions = f"""
+    Hi my name is {name}
+    I chose to {entree} as my main meal!
+    To go with it, I chose {side_one}, {side_two} as my sides.
+    And the best part, I have {dessert_one}, {dessert_two}, and
+    {dessert_three} waiting for me for dessert!
+    Let's eat!
+"""
+print(dinner_decisions)
