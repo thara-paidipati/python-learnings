7e822818c6aac1481782ae71e694afa9a787ead7
thara-paidipati <tharapaidipati1@gmail.com>
2024-01-03 16:22:13 +0000

    adding day9 file

---
+++ day9programme.py
@@ -0,0 +1,46 @@
+first_name = "Thara"
+last_name = "Paidipati"
+
+print(first_name + last_name)
+
+#OR
+
+full_name = first_name + " " + last_name
+print(full_name)
+
+print(5 * "balloon")
+
+
+my_favorite_desserts = ['Cookies', 'Cake', 'Ice Cream', 'Donuts']
+citrus_fruits = ['Orange', 'Lemon', 'Grapefruit', 'Pomelo', 'Lime']
+bunnies_spotted = [3, 5, 2, 8, 4, 5, 4, 3, 3,]
+robot_anwers = [True, False, False, True, True]
+facts_about_thara = ['Thara', 'Paidipati', 13, True]
+
+citrus_fruits = ['Orange', 'Lemon', 'Grapefruit', 'Pomelo', 'Lime']
+more_citrus_fruits = ['Orange', 'Lemon', 'Grapefruit', 'Pomelo', 'Lime']
+citrus_fruits == more_citrus_fruits
+
+citrus_fruits = ['Orange', 'Lemon', 'Grapefruit', 'Pomelo', 'Lime']
+citrus_fruits[0]
+citrus_fruits[2:4]
+citrus_fruits[:4]
+citrus_fruits[2:]
+
+my_favorite_desserts = []
+my_favorite_desserts += ['Brownies', 'Muffins', 'Chocolate']
+
+'Pomelo' in citrus_fruits
+'Donuts' not in citrus_fruits
+'Lime' not in citrus_fruits
+
+my_favorite_desserts.append('Apple Pie')
+my_favorite_desserts.append('Creme Brulee')
+
+my_favorite_desserts.remove('Muffins')
+del my_favorite_desserts[1]
+del my_favorite_dessert[1:]
+
+my_favorite_desserts[1] = 'Pumpkin Pie'
+
+rgb_color = ('red', 'green', 'blue')
