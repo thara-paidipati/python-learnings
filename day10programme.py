58e818a93a40ad1d93578c242e0943066451f909
thara-paidipati <tharapaidipati1@gmail.com>
2024-01-05 21:50:36 +0000

    adding day10 file

---
+++ day10programme.py
@@ -0,0 +1,9 @@
+if mood == 'tired':
+    hit_snooze_button = True
+    print("Thara is tired. She hits the snooze button.")
+elif mood == 'well-rested':
+    get_out_of_bed = True
+    print("Thara is well-rested. She's already out of bed!")
+
+my_favorite_things = ['Pink', 3, 'Brownies', 'Running', 'Coding']
+print(f"These are Thara's favorite things: {my_favorite_things}")
