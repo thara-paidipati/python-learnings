33aa2eda0b0c647f4fa4afd6e5d54def83e4eefa
thara-paidipati <tharapaidipati1@gmail.com>
2024-01-06 18:51:14 +0000

    adding day11 file

---
+++ day11programme.py
@@ -0,0 +1,82 @@
+#Activity 1
+
+your_cloud_shapes = ['circle', 'turtle', 'dolphin', 'truck', 'apple', 'spoon']
+friend_cloud_shapes = ['apple', 'turtle', 'spoon', 'truc', 'circle', 'dolphin']
+
+if your_cloud_shapes[0] == friends_cloud_shapes[0]:
+    print("We saw the same shape!")
+elif your_cloud_shapes[0] != friends_cloud_shapes[0]:
+    print("We saw different shapes this time!")
+
+if your_cloud_shapes[1] == friends_cloud_shapes[1]:
+    print("We saw the same shape!")
+elif your_cloud_shapes[1] != friends_cloud_shapes[1]:
+    print("We saw different shapes this time!")
+
+if your_cloud_shapes[2] == friends_cloud_shapes[2]:
+    print("We saw the same shape!")
+elif your_cloud_shapes[2] != friends_cloud_shapes[2]:
+    print("We saw different shapes this time!")
+
+if your_cloud_shapes[3] == friends_cloud_shapes[3]:
+    print("We saw the same shape!")
+elif your_cloud_shapes[3] != friends_cloud_shapes[3]:
+    print("We saw different shapes this time!")
+
+if your_cloud_shapes[4] == friends_cloud_shapes[4]:
+    print("We saw the same shape!")
+elif your_cloud_shapes[4] != friends_cloud_shapes[4]:
+    print("We saw different shapes this time!")
+
+if your cloud shapes[5] == friends_cloud_shapes[5]:
+    print("We saw the same shape!")
+elif your_cloud_shapes[5] != friends_cloud_shapes[5]:
+    print("We saw different shapes this time!")
+
+
+
+#Activity 2
+
+random_items = ['basket', 'tennis', 'bread', 'table', 'ball', 'game', 'box']
+
+#Marie is playing pin-pong with her friends. Another friends, Pierre, says that ping-pong is called something different in his country. Can you form the other name for ping-pong using the random_items list?
+print(f"{random_items[3]} {random_items[1]}")
+
+#Andre is about to play tennis with some friends. He has his tennis racket, but he needs one more thing. Write some code to print out what he needs.
+print(f"{random_items[4]}")
+
+#Jean just baked some fresh bread. He wants to bring a few loaves home to share. What can you make from the random_items list that can help him carry his bread home?
+print(f"{random_items[6]}")
+
+#Christina is singing the words to a popular song that is usually sung at a baseball game. Can you finish the lyrics? "Take my out to the _______ _______!"
+print(f"{random_items[4]} {random_items[5]}")
+
+#Leslie is writing a story about her favorites sport. It involves a hoope, five players on each team, and a recognizabale orange ball with black stripes. Which sport is it?
+print(f"{random_items[0]} {random_items[4]}")
+
+#Julia just recieved one of the fresh loaves of bread from jean. Thanking him, she quickly puts the loaf she recieved in thsi item to keep it warm. Can you name the item?
+print(f"{random_items[5]}")
+
+#Mario has a lot of board game and video games. Luckily, he can store most of them in this item to keep his room nice and clean! Name the item.
+print(f"{random_items[5]}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
