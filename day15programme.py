2cdda2ec383305ac92500c85820690b4486fe352
thara-paidipati <tharapaidipati1@gmail.com>
2024-01-18 21:48:45 +0000

    adding day15 file

---
+++ day15programme.py
@@ -0,0 +1,20 @@
+
+numbers = [1, 2, 3, 4, 5]
+for number in numbers:
+    print(number + 2)
+
+for i in range(3):
+    print(i)
+
+for i in range(10,21):
+    print(i)
+
+for i in range(0, 101, 10):
+    print(i)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+i = 0
+while(i < len(numbers)):
+    if (numbers[1] + 2) < 20:
+        print(numbers[1] + 2)
+    i += 1
